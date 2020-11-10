0d1f9318ea18468a692b5176b261777420bc4de5
Dmitry Mikhalchev <logiccrash@mail.ru>
2020-11-10 12:03:32 +0200

    2

---
+++ Space_Shooter/Assets/JMO Assets/WarFX/Scripts/CFX_AutoDestructShuriken.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(ParticleSystem))]
+public class CFX_AutoDestructShuriken : MonoBehaviour
+{ 
+	void OnEnable()
+	{
+		StartCoroutine("CheckIfAlive");
+	}
+	
+	IEnumerator CheckIfAlive ()
+	{
+		while(true)
+		{
+			yield return new WaitForSeconds(0.5f);
+			if(!GetComponent<ParticleSystem>().IsAlive(true))
+			{
+
+					GameObject.Destroy(this.gameObject);
+				break;
+			}
+		}
+	}
+}
